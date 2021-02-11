2aee411e3d829dad8ac0e3e75a3913d452dc50dd
Shiddarth Bista <shiddarthbista@gmail.com>
2021-02-11 16:31:07 -0500

    variable

--- src/main/java/com/example/demo/StudentClass.java
+++ src/main/java/com/example/demo/StudentClass.java
@@ -1,2 +1,33 @@
-package com.example.demo;public class StudentClass {
+package com.example.demo;
+
+public class StudentClass {
+    private String name;
+    private int roll;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+
+    public char getGrade() {
+        return grade;
+    }
+
+    public void setGrade(char grade) {
+        this.grade = grade;
+    }
+
+    private char grade;
+
 }
